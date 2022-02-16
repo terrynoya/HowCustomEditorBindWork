099495c57d7262983cc3eec7587fd6eda4cd2b7d
yaojianzhou <terrynoya@gmail.com>
2022-02-16 20:24:01 +0800

    add assembly utility

---
+++ Assets/Scripts/HowCustomEditorBindWork/Editor/AssemblyUtility.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Reflection;
+using UnityEngine;
+
+namespace HowCustomEditorBindWork.Editor
+{
+    public static class AssemblyUtility
+    {
+        private static string[] userAssemblyPrefixes = new string[]
+        {
+            "Assembly-CSharp",
+            "Assembly-UnityScript",
+            "Assembly-Boo",
+            "Assembly-CSharp-Editor",
+            "Assembly-UnityScript-Editor",
+            "Assembly-Boo-Editor"
+        };
+        
+        static AssemblyUtility()
+        {
+            AppDomain.CurrentDomain.AssemblyLoad += OnAssemblyLoaded;
+        }
+
+        public static void Init()
+        {
+            Assembly[] assemblies = AppDomain.CurrentDomain.GetAssemblies();
+            foreach (var assembly in assemblies)
+            {
+                GetAssemblyFlag(assembly);    
+            }
+        }
+
+        public static void GetAssemblyFlag(Assembly assembly)
+        {
+            string text = assembly.FullName.ToLower(CultureInfo.InvariantCulture);
+            if (text.StartsWithAnyOf(userAssemblyPrefixes, StringComparison.InvariantCultureIgnoreCase))
+            {
+                Debug.Log($"{assembly.FullName} is user assembly");
+            }
+            // else
+            // {
+            //     Debug.Log($"{assembly.FullName}");
+            // }
+        }
+        
+        private static bool StartsWithAnyOf(this string str, IEnumerable<string> values, StringComparison comparisonType = StringComparison.CurrentCulture)
+        {
+            IList<string> list = values as IList<string>;
+            if (list == null)
+            {
+                foreach (string value in values)
+                {
+                    if (str.StartsWith(value, comparisonType))
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            }
+            int count = list.Count;
+            for (int i = 0; i < count; i++)
+            {
+                if (str.StartsWith(list[i], comparisonType))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private static void OnAssemblyLoaded(object sender, AssemblyLoadEventArgs evt)
+        {
+            //evt.LoadedAssembly
+        }
+    }
+}
